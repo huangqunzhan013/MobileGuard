223950a28cbd9e88895158b4e1d86d0e06a3aa83
huangqunzhan013 <1041684959@qq.com>
2017-09-11 18:58:45 +0800

    第二次修改

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -10,5 +10,6 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         //呃呃呃
+        //淡淡的
     }
 }
