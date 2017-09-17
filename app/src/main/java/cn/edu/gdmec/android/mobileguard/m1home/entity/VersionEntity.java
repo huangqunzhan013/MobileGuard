4481d7e02f1c8f93194b1e0b84c4b5c871f11507
huangqunzhan013 <1041684959@qq.com>
2017-09-17 19:17:38 +0800

    m1部分代码

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,11 @@
+package cn.edu.gdmec.android.mobileguard.m1home.entity;
+
+/**
+ * Created by Lenovo on 2017/9/16.
+ */
+
+public class VersionEntity {
+    public String versioncode;
+    public String description;
+    public String apkurl;
+}
