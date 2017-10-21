9ce4146889e9bb5c708512bdf04c4f6c0e8d9049
huangqunzhan013 <1041684959@qq.com>
2017-10-21 21:30:46 +0800

    防盗

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/App.java
@@ -0,0 +1,45 @@
+package cn.edu.gdmec.android.mobileguard;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.Log;
+
+/**
+ * Created by Lenovo on 2017/10/20.
+ */
+
+public class App extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        correctSIM();
+    }
+    public void correctSIM(){
+        //检查SIM卡是否发生变化
+        SharedPreferences sp = getSharedPreferences("config", Context.MODE_PRIVATE);
+        //获取防盗保护的状态
+        boolean protecting = sp.getBoolean("protecting",true);
+        if(protecting){
+            //获取SIM卡号码
+            String bindsim=sp.getString("sim","");
+            //得到手机现在的sim卡号
+            TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            String realsim = tm.getSimSerialNumber();
+            realsim="999";
+            if(bindsim.equals(realsim)){
+                Log.i("","sim卡未发生变化，还是您的手机");
+            }else {
+                Log.i("","SIM卡变化了");
+                String safenumber=sp.getString("safephone","");
+                if(!TextUtils.isEmpty(safenumber)){
+                    SmsManager smsManager=SmsManager.getDefault();
+                    smsManager.sendTextMessage(safenumber,null,"你的亲友手机的SIM卡已经被更换！",null,null);
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup2Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup2Activity.java
@@ -1,7 +1,13 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import cn.edu.gdmec.android.mobileguard.R;
 
@@ -9,21 +15,71 @@ import cn.edu.gdmec.android.mobileguard.R;
  * Created by Lenovo on 2017/10/14.
  */
 
-public class Setup2Activity extends BaseSetupActivity {
+public class Setup2Activity extends BaseSetupActivity implements View.OnClickListener{
+    private TelephonyManager mTelephonyManager;
+    private Button mBindSIMBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_2);
         ((RadioButton)findViewById(R.id.rb_second)).setChecked(true);
+        //获取电话管理器这个系统服务
+        mTelephonyManager= (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        //找到布局中SIM卡绑定这个按钮
+        mBindSIMBtn= (Button) findViewById(R.id.btn_bind_sim);
+        mBindSIMBtn.setOnClickListener(this);
+        if(isBind()){
+            mBindSIMBtn.setEnabled(false);
+        }else {
+            mBindSIMBtn.setEnabled(true);
+        }
+
+    }
+    private boolean isBind(){
+        String simString=sp.getString("sim",null);
+        if(TextUtils.isEmpty(simString)){
+            return false;
+        }
+        return true;
     }
 
     @Override
     public void showNext() {
+        if(!isBind()){
+            Toast.makeText(this,"您还没有绑定SIM卡！",Toast.LENGTH_LONG).show();
+            return;
+        }
         startActivityAndFinishSelf(Setup3Activity.class);
     }
 
     @Override
     public void showPre() {
         startActivityAndFinishSelf(Setup1Activity.class);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_bind_sim:
+                //绑定SIM卡
+                bindSIM();
+                break;
+        }
+    }
+    //绑定SIM卡方法
+    private void bindSIM(){
+        if(!isBind()){
+            //使用电话管理服务器来获取SIM卡号
+            String simSerialNumber=mTelephonyManager.getSimSerialNumber();
+            //存储SIM卡号
+            SharedPreferences.Editor edit=sp.edit();
+            edit.putString("sim",simSerialNumber);
+            edit.commit();
+            Toast.makeText(this,"SIM卡绑定成功！",Toast.LENGTH_LONG).show();
+            mBindSIMBtn.setEnabled(false);
+        }else {
+            Toast.makeText(this,"SIM卡已经绑定！",Toast.LENGTH_LONG).show();
+            mBindSIMBtn.setEnabled(false);
+        }
     }
 }
