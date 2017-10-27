f850adfba2525375473c4467b90a3289a3b9124c
huangqunzhan013 <1041684959@qq.com>
2017-10-28 01:04:50 +0800

    防盗

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -1,5 +1,7 @@
 package cn.edu.gdmec.android.mobileguard.m1home;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.provider.Settings;
@@ -18,6 +20,7 @@ import cn.edu.gdmec.android.mobileguard.m1home.adapter.HomeAdapter;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.LostFindActivity;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.InterPasswordDialog;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.dialog.SetupPasswordDialog;
+import cn.edu.gdmec.android.mobileguard.m2theftguard.receiver.MyDeviceAdminReceiver;
 import cn.edu.gdmec.android.mobileguard.m2theftguard.utils.MD5Utils;
 
 
@@ -26,6 +29,10 @@ public class HomeActivity extends AppCompatActivity {
     private long mExitTime;
     //存储手机防盗密码
     private SharedPreferences msharedPreferences;
+    //设备管理员
+    private DevicePolicyManager policyManager;
+    //申请权限
+    private ComponentName componentName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +57,18 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }
         });
+        policyManager= (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+        componentName=new ComponentName(this, MyDeviceAdminReceiver.class);
+        boolean active=policyManager.isAdminActive(componentName);
+        if(!active){
+            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN,componentName);
+            intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,"获取超级管理员权限，用于远程锁屏和清除数据");
+            startActivity(intent);
+        }
     }
+
+
 
     public void startActivity(Class<?> cls) {
         Intent intent = new Intent(HomeActivity.this, cls);
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/receiver/MyDeviceAdminReceiver.java
@@ -0,0 +1,17 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Lenovo on 2017/10/27.
+ */
+
+public class MyDeviceAdminReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
