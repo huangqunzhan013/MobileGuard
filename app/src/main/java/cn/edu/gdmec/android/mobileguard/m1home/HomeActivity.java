72e6b9c760529d39355b8241a8588387a5d8fede
huangqunzhan013 <1041684959@qq.com>
2017-12-16 23:51:56 +0800

    高级工具号码归属地

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -28,6 +28,7 @@ import cn.edu.gdmec.android.mobileguard.m5virusscan.VirusScanActivity;
 import cn.edu.gdmec.android.mobileguard.m6cleancache.CacheClearListActivity;
 import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.OperatorSetActivity;
 import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.TrafficMonitoringActivity;
+import cn.edu.gdmec.android.mobileguard.m9advancedtools.AdvancedToolsActivity;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -74,6 +75,9 @@ public class HomeActivity extends AppCompatActivity {
                         break;
                     case 6:
                         startActivity(TrafficMonitoringActivity.class);
+                        break;
+                    case 7:
+                        startActivity(AdvancedToolsActivity.class);
                         break;
                 }
             }
